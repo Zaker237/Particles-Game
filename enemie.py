baf5042f6c3d7cf017bb079554954c2969df784a
Alex <mboutchouangalex+github@gmail.com>
2023-03-13 21:56:40 +0100

    coins color

--- enemie.py
+++ enemie.py
@@ -4,16 +4,16 @@ import configs
 
 class Enemie(pygame.Rect):
     COLORS = {
-        1: "white",
-        2: "white",
-        3: "white",
-        4: "white",
-        5: "white",
-        6: "white",
-        7: "white",
-        8: "white",
-        9: "white",
-        10: "white"
+        1: pygame.Color(255, 0, 0),
+        2: pygame.Color(255, 26, 26),
+        3: pygame.Color(255, 51, 51),
+        4: pygame.Color(255, 77, 77),
+        5: pygame.Color(255, 102, 102),
+        6: pygame.Color(255, 128, 128),
+        7: pygame.Color(255, 153, 153),
+        8: pygame.Color(255, 179, 179),
+        9: pygame.Color(255, 204, 204),
+        10: pygame.Color(255, 255, 255)
     }
 
     COINS = {
