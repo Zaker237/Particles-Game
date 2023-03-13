51b8bfcb81b48c915d6951782fa544cd141ba4b9
Alex <mboutchouangalex+github@gmail.com>
2023-03-13 21:08:33 +0100

    game configs

---
+++ configs.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+WIDTH = 1000
+HEIGHT = 800
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+PLAYER_VEL = 8
+STAR_WIDTH = 15
+STAR_HEIGHT = 30
+STAR_VEL = 5
+BALL_VEL = 15
+
+BALL_SONG = Path("./songs/ball.mp3")
